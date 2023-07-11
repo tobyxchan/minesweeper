0050c1171b923586601d3bcc51a78b0ee8e308bd
tobyxchan <tobyxchan@gmail.com>
2023-07-11 13:02:57 +1000

    added interactivity and clearing of empty squares

--- js/main.js
+++ js/main.js
@@ -10,18 +10,11 @@ let width = 10;
 let squares = [];
 // Choose how many bombs here - may change later
 let bombAmount = 20;
+let gameOver = false;
 
 
 
 /*---- cached element references ----*/
-
-
-
-
-
-
-/*--------- event listeners ---------*/
-
 
 
 
@@ -45,14 +38,14 @@ function loadField() {
         const square = document.createElement('div');
 
         // Every time a square is made, assign a unique ID
-        
+        square.setAttribute('id', i);
         // Assign a class to each square based on bomb or not
         square.classList.add(playableArray[i]);
 
         field.appendChild(square);
         squares.push(square);
 
-        // Event listener in case of click
+        // Click function event listener
         square.addEventListener('click', function(event) {
             click(square);
         })
@@ -85,14 +78,88 @@ function loadField() {
         }
     }
 }
-// Invoke function
+// Invoking function
 loadField();
 
 
 
 // Function for clicking a square
 function click(square) {
+    let currentId = square.id;
+    // Break function if game is over or square is already checked
+    if (gameOver) return;
+    if (square.classList.contains('checked') || square.classList.contains('flagged')) return;
+
     if (square.classList.contains('bomb')) {
         alert('GAME OVER!');
+        gameOver = true;
+    } else {
+        // Get the number of adjacent bombs from the 'total'
+        let total = square.getAttribute('data');
+        // If no bombs, add the 'checked' class
+        if (total != 0) {
+            square.classList.add('checked');
+            square.innerHTML = total;
+            return;
+        }
+        checkSquare(square, currentId);
     }
+    // At the end, mark as checked
+    square.classList.add('checked');
+}
+
+
+
+
+// Function to check squares around clicked square
+function checkSquare(square, currentId) {
+    // Check if is on the left or right edge of the field
+    const onLeftEdge = (currentId % width === 0);
+    const onRightEdge = (currentId % width === width - 1);
+
+    // Ensure 0 bomb squares are cleared slightly after click
+    setTimeout(() => {
+        // Using recursion to keep checking for 0 bomb squares after click
+        if (currentId > 0 && !onLeftEdge) {
+            const newId = squares[parseInt(currentId) -1].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+          }
+          if (currentId > 9 && !onRightEdge) {
+            const newId = squares[parseInt(currentId) +1 -width].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+          }
+          if (currentId > 10) {
+            const newId = squares[parseInt(currentId -width)].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+          }
+          if (currentId > 11 && !onLeftEdge) {
+            const newId = squares[parseInt(currentId) -1 -width].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+          }
+          if (currentId < 98 && !onRightEdge) {
+            const newId = squares[parseInt(currentId)+1].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+          }
+          if (currentId < 90 && !onLeftEdge) {
+            const newId = squares[parseInt(currentId) -1 +width].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+          }
+          if (currentId < 88 && !onRightEdge) {
+            const newId = squares[parseInt(currentId) +1 +width].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+          }
+          if (currentId < 89) {
+            const newId = squares[parseInt(currentId) +width].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+          }
+    // Adding 10ms of time
+    }, 10)
 }
